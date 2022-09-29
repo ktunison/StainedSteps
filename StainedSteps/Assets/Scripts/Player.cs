8e69038c71d650c87135177c11c274154a1229e9
ktunison <ktunison1@mail.csuchico.edu>
2022-09-29 00:25:02 -0700

    Movement & Materials
    Created the Movement system and added some colors for objects to make it easier to notice you are moving

---
+++ StainedSteps/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f;
+    private Vector3 inputMovement;
+    private Rigidbody rigidbody;
+    public float jumpVal;
+
+    [SerializeField]
+    private bool isGrounded;
+
+    private int lives = 3;
+    private int fallDepth;
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(inputMovement * speed * Time.deltaTime);
+    }
+
+    public void WASD(InputAction.CallbackContext context)
+    {
+        Vector2 myVector = context.ReadValue<Vector2>();
+        inputMovement = new Vector3(myVector.x, 0, myVector.y);
+    }
+}
