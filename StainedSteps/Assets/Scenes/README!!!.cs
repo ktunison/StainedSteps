5d14b597bc9730bf84903de7de863fe933dad3fa
TanukigaharaShinko <89051123+TanukigaharaShinko@users.noreply.github.com>
2022-10-20 09:48:43 -0700

    Updated Spring 2 revision

---
+++ StainedSteps/Assets/Scenes/README!!!.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class README!!! : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //LEVEL 1: PLATFORMER
+    //LEVEL 2: PICK UP
+    //LEVEL 3: ENEMY DODGER
+    //LEVEL 4:MAZE
+    //LEVEL 5: COLOR SWITCHES
