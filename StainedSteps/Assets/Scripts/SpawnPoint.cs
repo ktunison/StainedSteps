409e9978d62a107109fc4761fd17e2814608b06d
ktunison <ktunison1@mail.csuchico.edu>
2022-10-25 11:46:32 -0700

    Scene transition
    Made it so the character will spawn in if there is no character already in the scene and fixed the scene transition

--- StainedSteps/Assets/Scripts/SpawnPoint.cs
+++ StainedSteps/Assets/Scripts/SpawnPoint.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
+    public GameObject playerobject;
+
+
     // Start is called before the first frame update
     void Start()
     {
         //grabs player from another scene and puts it in spawn position
-        GameObject.FindGameObjectWithTag("Player").transform.position = gameObject.transform.position;
+
+        if (GameObject.FindGameObjectWithTag("Player") == null)
+        {
+            GameObject.Instantiate(playerobject, gameObject.transform);
+        }
+        else
+        {
+            GameObject.FindGameObjectWithTag("Player").transform.position = gameObject.transform.position;
+        }
     }
 }
